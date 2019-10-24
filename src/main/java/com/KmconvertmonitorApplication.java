c69d385ef678f72d77b33768c9b67ba44b05a03e
sky5cai <1147861255@qq.com>
2019-10-24 11:18:56 +0800

    modify km转换服务

---
+++ src/main/java/com/KmconvertmonitorApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmconvertmonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmconvertmonitorApplication.class, args);
+    }
+
+}
